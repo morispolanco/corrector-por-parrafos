d61baf9353867cf42d03a18c80e9a087ab5231a1
morispolanco <morispolanco@gmail.com>
2023-09-15 16:47:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,42 +23,33 @@ else:
     archivo = st.file_uploader('Cargar archivo CSV', type=['csv'])
 
     if archivo:
-        # Leemos el archivo CSV en modo de texto
-        reader = csv.reader(archivo.decode('utf-8').splitlines())
-        filas = list(reader)
+        # Leemos el contenido del archivo y lo decodificamos como UTF-8
+        contenido = archivo.read().decode('utf-8')
+
+        # Creamos un objeto StringIO para leer el contenido como un archivo CSV
+        archivo_csv = csv.reader(contenido.splitlines())
+
+        # Convertimos el objeto CSV en una lista de filas
+        filas = list(archivo_csv)
 
         # Resto del código...
 
+        # Ejemplo de uso de la lista de filas
+        for fila in filas:
+            st.write(fila)
 
+        # Ejemplo de uso de la API de OpenAI
+        texto = "Este es un ejemplo de texto para corregir."
+        correccion = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=texto,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.7,
+            top_p=None,
+            frequency_penalty=None,
+            presence_penalty=None
+        )
 
-
-    # Agregamos un botón para iniciar la corrección
-    if st.button('Corregir'):
-        # Utilizamos la API de GPT-3 para corregir cada fila
-        resultados = []
-        for i, fila in enumerate(filas):
-            texto = fila[0]  # Suponemos que el texto se encuentra en la primera columna
-
-            # Utilizamos la API de OpenAI para corregir la gramática y la puntuación
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=texto,
-                temperature=0,
-                max_tokens=512,
-                n=1,
-                stop=None,
-                timeout=5
-            )
-            correccion = response.choices[0].text.strip()
-
-            # Agregamos la corrección a la lista de resultados
-            resultados.append(correccion)
-
-        # Guardamos los resultados en un archivo CSV
-        with open('correcciones.csv', 'w', newline='') as archivo_csv:
-            writer = csv.writer(archivo_csv)
-            writer.writerow(['Texto corregido'])
-            writer.writerows(resultados)
-
-        # Mostramos un botón para descargar el archivo CSV
-        st.download_button('Descargar correcciones', 'correcciones.csv', 'Click aquí para descargar el archivo CSV')
+        st.write(correccion.choices[0].text)
