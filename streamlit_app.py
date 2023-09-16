edd9d2b74acf7006d2eaf3acee98c906abe1407f
morispolanco <morispolanco@gmail.com>
2023-09-15 21:22:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,45 +34,47 @@ else:
         # Creamos una lista para almacenar los párrafos corregidos
         parrafos_corregidos = []
 
-        # Iteramos sobre los párrafos
-        for parrafo in parrafos:
-            # Corregimos el párrafo utilizando la API de OpenAI
-            correccion = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=parrafo,
-                max_tokens=100,
-                n=1,
-                stop=None,
-                temperature=0.7,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=0.0
-            )
+        # Agregamos un botón para iniciar la corrección
+        if st.button("Corregir"):
+            # Iteramos sobre los párrafos
+            for parrafo in parrafos:
+                # Corregimos el párrafo utilizando la API de OpenAI
+                correccion = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=parrafo,
+                    max_tokens=100,
+                    n=1,
+                    stop=None,
+                    temperature=0.7,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0
+                )
 
-            # Obtenemos el párrafo corregido
-            parrafo_corregido = correccion.choices[0].text
+                # Obtenemos el párrafo corregido
+                parrafo_corregido = correccion.choices[0].text
 
-            # Agregamos el párrafo corregido a la lista
-            parrafos_corregidos.append(parrafo_corregido)
+                # Agregamos el párrafo corregido a la lista
+                parrafos_corregidos.append(parrafo_corregido)
 
-        # Creamos un archivo CSV para almacenar los párrafos corregidos
-        with open("resultado.csv", "w", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Párrafo corregido"])
-            writer.writerows(zip(parrafos_corregidos))
+            # Creamos un archivo CSV para almacenar los párrafos corregidos
+            with open("resultado.csv", "w", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow(["Párrafo corregido"])
+                writer.writerows(zip(parrafos_corregidos))
 
-        # Mostramos el enlace para descargar el archivo CSV
-        st.subheader("Archivo CSV generado:")
-        st.markdown("[Descargar resultado CSV](resultado.csv)")
+            # Mostramos el enlace para descargar el archivo CSV
+            st.subheader("Archivo CSV generado:")
+            st.markdown("[Descargar resultado CSV](resultado.csv)")
 
-        # Creamos un nuevo archivo DOC con los párrafos corregidos
-        doc_corregido = docx.Document()
-        for parrafo_corregido in parrafos_corregidos:
-            doc_corregido.add_paragraph(parrafo_corregido)
+            # Creamos un nuevo archivo DOC con los párrafos corregidos
+            doc_corregido = docx.Document()
+            for parrafo_corregido in parrafos_corregidos:
+                doc_corregido.add_paragraph(parrafo_corregido)
 
-        # Guardamos el documento corregido en un archivo .docx
-        doc_corregido.save("resultado.docx")
+            # Guardamos el documento corregido en un archivo .docx
+            doc_corregido.save("resultado.docx")
 
-        # Mostramos el enlace para descargar el archivo DOC
-        st.subheader("Archivo DOC generado:")
-        st.markdown("[Descargar resultado DOC](resultado.docx)")
+            # Mostramos el enlace para descargar el archivo DOC
+            st.subheader("Archivo DOC generado:")
+            st.markdown("[Descargar resultado DOC](resultado.docx)")
