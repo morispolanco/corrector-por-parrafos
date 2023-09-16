9371071346d37438e9d63d0bcb1737c186eedd71
morispolanco <morispolanco@gmail.com>
2023-09-15 21:37:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,24 +21,25 @@ if not api_key:
     st.warning("Please enter a valid API key to continue.")
 else:
     openai.api_key = api_key
-
+    
 # Streamlit App
 st.title('DaVinci-003 Grammar and Style Correction for DOCX')
 
 uploaded_file = st.file_uploader("Upload DOCX file", type='docx')
 
-if uploaded_file is not None:
-    df = docx_to_df(uploaded_file)
-    corrected_document = Document() # This document will contain corrected text
-    for index, row in df.iterrows():
-        prompt=row['text']
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=150
-        )
-        corrected_document.add_paragraph(response.choices[0].text.strip())
-    corrected_document.save("corrected.docx")
-    st.success('Done writing corrected document.')
-    st.download_button('Download corrected DOCX', 'corrected.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+if uploaded_file is not None and st.button('Corregir'):
+    with st.spinner("Corrigiendo..."):
+        df = docx_to_df(uploaded_file)
+        corrected_document = Document() # This document will contain corrected text
+        for index, row in df.iterrows():
+            prompt=row['text']
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                temperature=0.5,
+                max_tokens=150
+            )
+            corrected_document.add_paragraph(response.choices[0].text.strip())
+        corrected_document.save("corrected.docx")
+        st.success('Se ha completado la corrección del documento.')
+        st.download_button('Descargar documento corregido DOCX', 'corrected.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
